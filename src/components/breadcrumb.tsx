3b22183a5f374b1edf5c9069a837f7f8fc4bd9ba
Rostyslav Vozniak <rostik19wozniak@gmail.com>
2025-08-19 17:43:01 +0200

    fix text wrap

--- src/components/breadcrumb.tsx
+++ src/components/breadcrumb.tsx
@@ -27,51 +27,54 @@ export function Breadcrumb() {
     .map((crumb) => unslugify(crumb));
 
   return (
-    <ShadcnBreadcrumb className="scrollbar-hide overflow-x-auto">
-      <BreadcrumbList className="w-full flex-nowrap px-4">
-        <BreadcrumbItem>
-          <BreadcrumbLink asChild>
-            <Link
-              href="/"
-              className="flex items-center gap-x-2"
-              aria-label="Przejdź do strony głównej"
-            >
-              <HomeIcon /> Strona główna
-            </Link>
-          </BreadcrumbLink>
-        </BreadcrumbItem>
+    <div className="relative">
+      <ShadcnBreadcrumb className="scrollbar-hide overflow-x-auto">
+        <BreadcrumbList className="w-full flex-nowrap px-4">
+          <BreadcrumbItem>
+            <BreadcrumbLink asChild>
+              <Link
+                href="/"
+                className="flex items-center gap-x-2 text-nowrap"
+                aria-label="Przejdź do strony głównej"
+              >
+                <HomeIcon /> Strona główna
+              </Link>
+            </BreadcrumbLink>
+          </BreadcrumbItem>
 
-        {crumbs.map((crumb, index) => {
-          currentLink += `/${crumb}`;
-          const name = crumb.replaceAll("-", " ");
-          return (
-            <Fragment key={index}>
-              <BreadcrumbSeparator />
-              {index < crumbs.length - 1 ? (
-                <>
-                  <BreadcrumbItem key={index}>
-                    <BreadcrumbLink asChild>
-                      <Link
-                        href={currentLink}
-                        className="capitalize"
-                        aria-label={`Przejdź do strony ${name}`}
-                      >
-                        {name}
-                      </Link>
-                    </BreadcrumbLink>
+          {crumbs.map((crumb, index) => {
+            currentLink += `/${crumb}`;
+            const name = crumb.replaceAll("-", " ");
+            return (
+              <Fragment key={index}>
+                <BreadcrumbSeparator />
+                {index < crumbs.length - 1 ? (
+                  <>
+                    <BreadcrumbItem key={index}>
+                      <BreadcrumbLink asChild>
+                        <Link
+                          href={currentLink}
+                          className="capitalize"
+                          aria-label={`Przejdź do strony ${name}`}
+                        >
+                          {name}
+                        </Link>
+                      </BreadcrumbLink>
+                    </BreadcrumbItem>
+                  </>
+                ) : (
+                  <BreadcrumbItem>
+                    <BreadcrumbPage className="text-nowrap pr-4 capitalize">
+                      {crumb}
+                    </BreadcrumbPage>
                   </BreadcrumbItem>
-                </>
-              ) : (
-                <BreadcrumbItem>
-                  <BreadcrumbPage className="text-nowrap pr-4 capitalize">
-                    {crumb}
-                  </BreadcrumbPage>
-                </BreadcrumbItem>
-              )}
-            </Fragment>
-          );
-        })}
-      </BreadcrumbList>
-    </ShadcnBreadcrumb>
+                )}
+              </Fragment>
+            );
+          })}
+        </BreadcrumbList>
+      </ShadcnBreadcrumb>
+      <div className="absolute bottom-0 right-0 top-0 w-8 bg-gradient-to-r from-transparent to-background"></div>
+    </div>
   );
 }
